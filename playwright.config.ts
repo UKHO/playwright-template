54867ff5405fcb82138b831b5d41ef759f752a70
Sara Gowen <sara.gowen@ukho.gov.uk>
2022-04-21 19:27:02 +0100

    Added screenshots

--- playwright.config.ts
+++ playwright.config.ts
@@ -23,8 +23,8 @@ const config: PlaywrightTestConfig = {
   },
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env['CI'],
-  /* Retry on CI only */
-  retries: process.env['CI'] ? 2 : 0,
+  /* Retry on CI only - this will allow us to record a trace*/
+  retries: process.env['CI'] ? 1 : 0,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: process.env['CI'] ? [['junit', { outputFile: 'uiTestResults.xml' }]] : [['html']],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
@@ -33,6 +33,7 @@ const config: PlaywrightTestConfig = {
     actionTimeout: 0,
     /* Base URL to use in actions like `await page.goto('/')`. */
     // baseURL: 'http://localhost:3000',
+    screenshot: 'only-on-failure',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
