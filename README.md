a0bdccc0d661aab7168a80b5f12ffc3505d83273
Sara Gowen <9001998+dynamictulip@users.noreply.github.com>
2022-04-22 14:52:29 +0100

    Set up CI with Azure Pipelines (#7)
    * Set up CI with Azure Pipelines
    
    * Added screenshots
    
    * Updated readme
    
    * Skip failing test

--- README.md
+++ README.md
@@ -16,3 +16,9 @@ See [the Playwright command line docs](https://playwright.dev/docs/test-cli) for
 From the project directory run `ng serve` to spin up the web server and navigate to <http://localhost:4200/> - you don't need to do this before running tests as that's handled by the Playwright Test framework.
 
 The app will automatically reload if you change any of the source files.
+
+## Pipeline
+
+The Azure Devops pipeline is configured to install prerequisites, run the tests, publish test results and then publish test result attachments as a pipeline artifact.
+
+When a test fails in the pipeline it is retried and a trace is recorded of the retry - this trace can be analysed to see the entire flow of the test including the full dom before and after each Playwright action. See [Playwright Trace docs](https://playwright.dev/docs/trace-viewer) for information on how to view the trace.
--- playwright.config.ts
+++ playwright.config.ts
@@ -22,20 +22,16 @@ const config: PlaywrightTestConfig = {
     timeout: 5000
   },
   /* Fail the build on CI if you accidentally left test.only in the source code. */
-  forbidOnly: !!process.env.CI,
-  /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
-  /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
+  forbidOnly: !!process.env['CI'],
+  /* Retry on CI only - this will allow us to record a trace*/
+  retries: process.env['CI'] ? 1 : 0,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: process.env['CI'] ? [['junit', { outputFile: 'uiTestResults.xml' }]] : [['html']],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
     actionTimeout: 0,
-    /* Base URL to use in actions like `await page.goto('/')`. */
-    // baseURL: 'http://localhost:3000',
-
+    screenshot: 'only-on-failure',
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
   },
@@ -92,15 +88,12 @@ const config: PlaywrightTestConfig = {
     // },
   ],
 
-  /* Folder for test artifacts such as screenshots, videos, traces, etc. */
-  // outputDir: 'test-results/',
-
   /* Run your local dev server before starting the tests */
-   webServer: {
-     command: 'npm run start',
-     port: 4200,
-     reuseExistingServer: true
-   },
+  webServer: {
+    command: 'npm run start',
+    port: 4200,
+    reuseExistingServer: true
+  },
 };
 
 export default config;
