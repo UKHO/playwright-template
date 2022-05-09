d6816e7d91083df821df03c2c047c8431db0063a
Sara Gowen <sara.gowen@ukho.gov.uk>
2022-05-09 11:57:06 +0100

    Use POM to navigate to page in order to handle random popup

--- tests/global-setup.ts
+++ tests/global-setup.ts
@@ -4,15 +4,14 @@ import { LoginPage } from './page-object-model/loginpage';
 
 async function globalSetup(config: FullConfig) {
     const browser = await chromium.launch();
-    const page = await browser.newPage();
+    //######### TODO: Replace this with config ###############
+    const page = await browser.newPage({ baseURL: 'http://localhost:4200' });
 
     // This saves a clean slate which we can use in tests that require us not to be logged in
     await page.context().storageState({ path: 'cleanStorageState.json' });
 
-//######### Replace this with config ###############
-    await page.goto('http://localhost:4200/login');
-
     let loginPage = new LoginPage(page);
+    await loginPage.navigateTo();
     await loginPage.fillFormWithValidDetails();
     await loginPage.submitForm();
 
