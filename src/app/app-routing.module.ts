16bdd2892f4e4580ed4d5e11e925f90286cd1d9a
Sara Gowen <sara.gowen@ukho.gov.uk>
2022-05-09 11:39:09 +0100

    When logged in redirect login page to welcome page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,12 @@ import { AuthGuard } from './auth.guard';
 import { FormResultsComponent } from './form/form-results.component';
 import { MyFormComponent } from './form/my-form.component';
 import { LoginComponent } from './login/login.component';
+import { LoginPageGuard } from './login/loginpage.guard';
 import { WelcomeComponent } from './welcome/welcome.component';
 
 const routes: Routes = [
   {path: '', redirectTo: 'welcome', pathMatch: 'full'},
-  {path: 'login', component: LoginComponent},
+  {path: 'login', component: LoginComponent, canActivate: [LoginPageGuard]},
   {path: 'welcome', component: WelcomeComponent},
   {path: 'form', component: MyFormComponent, canActivate: [AuthGuard]},
   {path: 'form-results', component: FormResultsComponent, canActivate: [AuthGuard]},
