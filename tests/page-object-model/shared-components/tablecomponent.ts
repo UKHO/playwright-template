998f73c5a0e276c3870cb34f33a8ae8a4f7325f4
Sara Gowen <sara.gowen@ukho.gov.uk>
2022-07-25 13:01:31 +0100

    Added table row test

--- tests/page-object-model/shared-components/tablecomponent.ts
+++ tests/page-object-model/shared-components/tablecomponent.ts
@@ -1,10 +1,19 @@
-import { Page, expect } from "@playwright/test";
+import { Page, expect, Locator } from "@playwright/test";
 
 export class TableComponent {
     readonly expect: TableComponentAssertions;
+    readonly tableComponentLocator: Locator
+    readonly rowsLocator: Locator
 
     constructor(readonly page: Page) {
         this.expect = new TableComponentAssertions(this);
+
+        this.tableComponentLocator = page.locator("app-form-results");
+        this.rowsLocator = this.tableComponentLocator.locator("tr");
+    }
+
+    cellLocator(row: number, column: number): Locator {
+        return this.rowsLocator.nth(row).locator('td').nth(column);
     }
 }
 
@@ -12,5 +21,26 @@ class TableComponentAssertions {
     constructor(readonly tableComponent: TableComponent) {
     }
 
+    async toOnlyHaveValues(values: { field: string; answer: string; }[]): Promise<void> {
+        await expect(this.tableComponent.rowsLocator, 'table row count should match expected values count').toHaveCount(values.length);
 
+        const actualRowCount = await this.tableComponent.rowsLocator.count();
+
+        for (const expectedRow of values) {
+            let foundMatch = false;
+            for (let i = 0; i < actualRowCount; i++) {
+                const fieldCellLocator = this.tableComponent.cellLocator(i, 0);
+                const answerCellLocator = this.tableComponent.cellLocator(i, 1);
+
+                const actualFieldContent = await fieldCellLocator.textContent();
+                if (actualFieldContent === expectedRow.field) {
+                    await expect(answerCellLocator, 'expected answer for [' + expectedRow.field + '] to be [' + expectedRow.answer + ']').toHaveText(expectedRow.answer);
+                    foundMatch = true;
+                    break;
+                }
+            }
+
+            expect(foundMatch, 'expected to find row [' + expectedRow.field + ', ' + expectedRow.answer + ']').toBeTruthy();
+        }
+    }
 }
